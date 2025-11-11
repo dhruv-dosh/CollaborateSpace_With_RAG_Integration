724317b7cd9584cbefeea1b97972cdfdfe4c43c2
dhruv-dosh <dhruvdoshi.pvt@gmail.com>
2025-11-12 03:46:07 +0530

    Updating

--- CSpacebackend/src/main/java/com/Dhruv/CollaborateSpaceApplication.java
+++ CSpacebackend/src/main/java/com/Dhruv/CollaborateSpaceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectmanagementApplication {
+public class CollaborateSpaceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectmanagementApplication.class, args);
+		SpringApplication.run(CollaborateSpaceApplication.class, args);
 	}
 
 }
