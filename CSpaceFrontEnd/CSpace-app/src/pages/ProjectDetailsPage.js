1cabdcada3955c5706a266d1547ae825ecd96deb
dhruv-dosh <dhruvdoshi.pvt@gmail.com>
2025-11-12 20:47:30 +0530

    updating

--- CSpaceFrontEnd/CSpace-app/src/pages/ProjectDetailsPage.js
+++ CSpaceFrontEnd/CSpace-app/src/pages/ProjectDetailsPage.js
@@ -112,7 +112,7 @@ const ProjectDetailsPage = () => {
                 }`}
             >
               <span className="flex items-center gap-2">
-                Documents
+                Ask Questions
                 <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4z" clipRule="evenodd" />
                 </svg>
